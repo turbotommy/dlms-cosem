62587d3ccc1622547096ee7e6837a99df4f85f82
Tommy Ekh <Tommy.Ekh@coop.se>
2023-09-05 15:12:27 +0200

    Newest changes

--- examples/parse_norwegian_han.py
+++ examples/parse_norwegian_han.py
@@ -1,4 +1,5 @@
 import pprint
+import paho.mqtt.client as paho
 import sys
 sys.path.append(r'C:\Lab\dlms-cosem')
 from dlms_cosem.cosem import Obis
@@ -31,15 +32,86 @@ hdlc_data_hex3 = (
     "7ea11d01000110b0aee6e7000f4000000000022409060100000281ff09074b464d5f30303109060000600100ff09103733343031353730333037333230343409060000600107ff09074d41333034483409060100010700ff060000024a09060100020700ff060000000009060100030700ff060000000009060100040700ff0600000109090601001f0700ff060000012d09060100330700ff060000056909060100470700ff06000005ef09060100200700ff060000090f09060100340700ff060000090709060100480700ff060000092009060000010000ff090c07e7081b0715202dffffc40009060100010800ff060107d8d009060100020800ff060000000009060100030800ff060000171e09060100040800ff06003bfaf201477e"
 )
 
+#clock, status = datetime_from_bytes(b"570656732659")
+
 #crc=frames.CRCCCITT()
 #tst=crc.calculate_for(bytes.fromhex(    "a11f010001f693aee6e700"))
+def handle_msg(client, userdata, msg):
+    print(bytes.hex(msg.payload))
+    bArray=bytearray(msg.payload)
+    bArray+=b'\x7e'
+    ui = frames.UnnumberedInformationFrame.from_bytes(bArray)
+    dn = xdlms.DataNotification.from_bytes(
+        ui.payload[4:]
+    )  # The first 3 bytes should be ignored. Even 4 for KAIFA
 
-ui = frames.UnnumberedInformationFrame.from_bytes(bytes.fromhex(hdlc_data_hex3))
-dn = xdlms.DataNotification.from_bytes(
-    ui.payload[4:]
-)  # The first 3 bytes should be ignored. Even 4 for KAIFA
+    result = parse_as_dlms_data(dn.body)
 
-result = parse_as_dlms_data(dn.body)
+    obisStr=""
+    # rest is data
+    for item in result:
+        try:
+            if(len(obisStr)>0):
+                print(obisStr)
+                if(obisStr=="0-0:1.0.0.255"):
+                    clock,status=datetime_from_bytes(item)
+                    print(clock.isoformat)
+                elif(obisStr=="1-0:1.7.0.255"):
+                    print("Active power+")
+                elif(obisStr=="1-0:2.7.0.255"):
+                    print("Active power-")
+                elif(obisStr=="1-0:3.7.0.255"):
+                    print("ReActive power+")
+                elif(obisStr=="1-0:4.7.0.255"):
+                    print("ReActive power-")
+                elif(obisStr=="1-0:31.7.0.255"):
+                    print("Ström L1: {0}".format(item/100))
+                elif(obisStr=="1-0:51.7.0.255"):
+                    print("Ström L2: {0}".format(item/100))
+                elif(obisStr=="1-0:71.7.0.255"):
+                    print("Ström L3: {0}".format(item/100))
+                elif(obisStr=="1-0:32.7.0.255"):
+                    print("Spänning L1: {0}".format(item/10))
+                elif(obisStr=="1-0:52.7.0.255"):
+                    print("Spänning L2: {0}".format(item/10))
+                elif(obisStr=="1-0:72.7.0.255"):
+                    print("Spänning L3: {0}".format(item/10))
+                elif(obisStr=="1-0:1.8.0.255"):
+                    print("Total meter: {0}".format(item))
+                    
+                
+            obisStr=''        
+            obis = Obis.from_bytes(item)
+            obisStr=obis.to_string()
+        except Exception:
+            print(item)
+
+    clock, status = datetime_from_bytes(b"570656732659")
+    print(clock)
+#    for b in msg.payload:
+#        print(hex(b))
+
+client=paho.Client()
+client.on_message=handle_msg
+client.username_pw_set("appuser", "appuser")
+if client.connect("misteln13",1883,) != 0:
+    print("Not able to connect to broker")
+    sys.exit(1)
+
+#client.disconnect()
+
+client.subscribe("malarenergi/raw")
+
+try:
+    print("Ctrl+C to exit")
+    client.loop_forever()
+except Exception:
+    print(Exception)
+finally:
+    print("Disconnecting")
+    client.disconnect
+
+
 # pprint.pprint(result)
 # First is date
 # date_row = result.pop(0)
@@ -47,33 +119,3 @@ result = parse_as_dlms_data(dn.body)
 # clock, stats = datetime_from_bytes(date_row[1])
 # print(f"Clock object: {clock_obis.to_string()}, datetime={clock}")
 
-obisStr=""
-# rest is data
-for item in result:
-    try:
-        if(len(obisStr)>0):
-            print(obisStr)
-            if(obisStr=="0-0:1.0.0.255"):
-                clock,status=datetime_from_bytes(item)
-            elif(obisStr=="1-0:1.7.0.255"):
-                print("Active power+")
-            elif(obisStr=="1-0:2.7.0.255"):
-                print("Active power-")
-            elif(obisStr=="1-0:3.7.0.255"):
-                print("ReActive power+")
-            elif(obisStr=="1-0:4.7.0.255"):
-                print("ReActive power-")
-            elif(obisStr=="1-0:31.7.0.255"):
-                print("Ström L1")
-            elif(obisStr=="1-0:51.7.0.255"):
-                print("Ström L2")
-            elif(obisStr=="1-0:71.7.0.255"):
-                print("Ström L3")
-        obisStr=''        
-        obis = Obis.from_bytes(item)
-        obisStr=obis.to_string()
-    except Exception:
-        print(item)
-
-clock, status = datetime_from_bytes(b"5706567326590407")
-print(clock)
